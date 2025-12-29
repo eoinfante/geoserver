8be2f31131ab20875d8f61c2b333aa4b5eaa1258
eoinfante <eoinfante21@gmail.com>
2025-12-28 23:47:35 -0500

    fix cors again

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
+//use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prepend(HandleCors::class);
+        $middleware->use([
+            // ... other global middleware
+            \Fruitcake\Cors\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

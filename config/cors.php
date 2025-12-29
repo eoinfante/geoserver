1184d83b4d95327116d0be736d5f1f73229582bb
eoinfante <eoinfante21@gmail.com>
2025-12-28 23:51:22 -0500

    fix cors one more time

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'   => ['*'],
     'allowed_origins'   => [
-        'https://geoserver-main-vzwrph.laravel.cloud',
+        'https://geoserver-main-vzwrph.laravel.cloud/*',
     ],
     'allowed_headers'   => ['*'],
     'exposed_headers'   => [],

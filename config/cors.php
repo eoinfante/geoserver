fd4577faf155b6f1a1f2f0a4f11091436226e436
eoinfante <eoinfante21@gmail.com>
2025-12-28 23:56:34 -0500

    fix cors three  more time

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'   => ['*'],
     'allowed_origins'   => [
-        '*',
+        'https://geoserver-main-vzwrph.laravel.cloud',
+        'https://geoserver-main-vzwrph.laravel.cloud/api',
+        'https://geoserver-main-vzwrph.laravel.cloud/api/check-auth',
     ],
     'allowed_headers'   => ['*'],
     'exposed_headers'   => [],
